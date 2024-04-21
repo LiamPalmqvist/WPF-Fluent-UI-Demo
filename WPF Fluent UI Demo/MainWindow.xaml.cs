e88747510cdace0f5a797ea710934e8791b857c7
Liam Palmqvist <Liam.palmqvist@icloud.com>
2024-04-21 23:22:01 +0100

    Initial commit

--- WPF Fluent UI Demo/MainWindow.xaml.cs
+++ WPF Fluent UI Demo/MainWindow.xaml.cs
@@ -14,7 +14,7 @@ namespace WPF_Fluent_UI_Demo
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
         public MainWindow()
         {
